10189f6e23830a9d4c216f8af92104457a9fd4d6
quxianwei <quxw@goldencis.com>
2024-06-13 15:39:13 +0800

    :art: 添加网关、依赖模块

---
+++ spring-cloud-gateway/src/main/java/online/k12code/gateway/SpringCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package online.k12code.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGatewayApplication.class, args);
+    }
+
+}
