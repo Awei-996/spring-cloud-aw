52951e0027402757632ab3ba0520b4e1bab571a4
quxianwei <quxw@goldencis.com>
2024-06-13 17:25:32 +0800

    :memo: 添加代码格式

--- spring-cloud-gateway/src/main/java/online/k12code/gateway/SpringCloudGatewayApplication.java
+++ spring-cloud-gateway/src/main/java/online/k12code/gateway/SpringCloudGatewayApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringCloudGatewayApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringCloudGatewayApplication.class, args);
-    }
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringCloudGatewayApplication.class, args);
+	}
 
 }
